765a1c25d4daa211f1e29d1d497a8073b6a53a0c
Alan Kagazov <alan.kagazov@gmail.com>
2026-02-04 22:12:56 +0500

    Добавлены примеры if с разными знаками в if_statement.py

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,42 @@
+#If statement
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+
+# Example 1
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a")  # True
+
+# Example 2
+x = 52
+y = 67
+if x < y:
+    print("x is less than y")  # True
+
+# Example 3
+p = 100
+q = 100
+if p >= q:
+    print("p is greater than or equal to q")  # True
+
+# Example 4
+m = -10
+n = -5
+if m <= n:
+    print("m is less than or equal to n")  # True
+
+# Example 5
+num1 = 7
+num2 = 10
+if num1 != num2:
+    print("num1 is not equal to num2")  # True
+
+
+
+
+
