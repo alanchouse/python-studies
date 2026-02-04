1bc96cfc76bdef68a896ebe2874ac7e7cbadc8ee
Alan Kagazov <alan.kagazov@gmail.com>
2026-02-04 21:46:23 +0500

    Добавлен код в boolean_comparison.py

---
+++ Practice2/boolean/boolean_comparison.py
@@ -0,0 +1,62 @@
+#Comparison operators
+
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+
+# Example 1
+x = 52
+y = 67
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # False
+print(x < y)    # True
+print(x >= y)   # False
+print(x <= y)   # True
+
+# Example 2
+x = 10000
+y = -10000
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # True
+print(x < y)    # False
+print(x >= y)   # True
+print(x <= y)   # False
+
+# Example 3
+x = 152
+y = 885
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # False
+print(x < y)    # True
+print(x >= y)   # False
+print(x <= y)   # True
+
+# Example 4
+x = -10000
+y = 52
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # False
+print(x < y)    # True
+print(x >= y)   # False
+print(x <= y)   # True
+
+# Example 5
+x = 885
+y = 10000
+print(x == y)   # False
+print(x != y)   # True
+print(x > y)    # False
+print(x < y)    # True
+print(x >= y)   # False
+print(x <= y)   # True
