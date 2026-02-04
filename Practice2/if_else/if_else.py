8d1f74142b45e300493a2cee1e30d6bebf708ada
Alan Kagazov <alan.kagazov@gmail.com>
2026-02-04 22:25:27 +0500

    Добавлены примеры if else в if_else.py

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,51 @@
+#if_else
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+# Example 1
+a = 75
+b = 150
+if a > b:
+    print("a is bigger than b")
+else:
+    print("a is smaller than or equal to b")  # True
+
+# Example 2
+x = 52
+y = 67
+if x < y:
+    print("x is less than y")  # True
+else:
+    print("x is not less than y")
+
+# Example 3
+p = 100
+q = 100
+if p != q:
+    print("p and q are different")
+else:
+    print("p and q are equal")  # True
+
+# Example 4
+num = -5
+check = 0
+if num >= check:
+    print("num is zero or positive")
+else:
+    print("num is negative")  # True
+
+# Example 5
+score = 85
+passing = 90
+if score >= passing:
+    print("You passed")
+else:
+    print("You failed")  # True
+
+
