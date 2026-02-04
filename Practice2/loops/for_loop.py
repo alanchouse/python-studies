b4d4ee3e59f5397ab918ef63774350f03ca8ad04
Alan Kagazov <alan.kagazov@gmail.com>
2026-02-04 22:51:48 +0500

    Добавлены примеры обычного for в for_loop.py

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,31 @@
+#for_loop
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# Example 1
+numbers = [10, 20, 30, 40]
+for num in numbers:
+    print(num)  # Выведет 10, 20, 30, 40
+
+# Example 2
+veggies = ["carrot", "broccoli", "spinach"]
+for veg in veggies:
+    print(veg)  # Выведет carrot, broccoli, spinach
+
+# Example 3
+letters = ["a", "b", "c", "d"]
+for letter in letters:
+    print(letter)  # Выведет a, b, c, d
+
+# Example 4
+colors = ["red", "green", "blue"]
+for color in colors:
+    print(color)  # Выведет red, green, blue
+
+# Example 5
+animals = ["cat", "dog", "rabbit"]
+for animal in animals:
+    print(animal)  # Выведет cat, dog, rabbit
