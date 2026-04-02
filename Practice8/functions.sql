78ee5d9345b1b290304596737419b0778c81e194
Alan Kagazov <alan.kagazov@gmail.com>
2026-04-02 13:36:35 +0500

    Add Practice8 - PhoneBook with functions and stored procedures

---
+++ Practice8/functions.sql
@@ -0,0 +1,21 @@
+
+CREATE OR REPLACE FUNCTION search_contacts(p TEXT)
+RETURNS TABLE(name VARCHAR, phone VARCHAR) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT name, phone FROM contacts
+    WHERE name ILIKE '%' || p || '%'
+       OR phone ILIKE '%' || p || '%';
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION get_contacts_paginated(lim INT, off INT)
+RETURNS TABLE(id INT, name VARCHAR, phone VARCHAR) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT * FROM contacts
+    LIMIT lim OFFSET off;
+END;
+$$ LANGUAGE plpgsql;
