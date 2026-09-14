1f4057615577409ab7eaada6affa1df54ce0533f
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:03:48 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/casting.py
+++ Practice1/python-basics/casting.py
@@ -2,3 +2,29 @@ x = int(1)
 y = int(2.8)
 z = str(3)
 print(x, y, z)
+
+
+x = int(5)
+y = int(7.9)
+z = str(10)
+print(x, y, z)
+
+a = int(10.5)
+b = float(3)
+c = str(20)
+print(a, b, c)
+
+p = int(15)
+q = float(6.7)
+r = str(25)
+print(p, q, r)
+
+num1 = int(8.9)
+num2 = int(4)
+num3 = str(12)
+print(num1, num2, num3)
+
+a = int(20)
+b = float(5.5)
+c = str(30)
+print(a, b, c)
