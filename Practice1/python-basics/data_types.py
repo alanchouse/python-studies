6fc5bfb1e32f35f38582b73c740f1d2e69057e4b
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:13:34 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/data_types.py
+++ Practice1/python-basics/data_types.py
@@ -2,3 +2,28 @@ x = 5
 y = "Hello"
 print(type(x))
 print(type(y))
+
+x = 10
+y = "Python"
+print(type(x))
+print(type(y))
+
+a = 3.5
+b = "Hello"
+print(type(a))
+print(type(b))
+
+age = 18
+name = "Alan"
+print(type(age))
+print(type(name))
+
+price = 99.9
+city = "Almaty"
+print(type(price))
+print(type(city))
+
+is_student = True
+number = 25
+print(type(is_student))
+print(type(number))
