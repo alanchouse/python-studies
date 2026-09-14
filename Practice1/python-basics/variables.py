4f0185a97913138da3a8b655ab1883558bcc8a10
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:26:41 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/variables.py
+++ Practice1/python-basics/variables.py
@@ -2,3 +2,28 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+x = 10
+y = "Alan"
+print(x)
+print(y)
+
+a = 20
+b = "Python"
+print(a)
+print(b)
+
+num = 15
+name = "John"
+print(num)
+print(name)
+
+age = 18
+city = "Almaty"
+print(age)
+print(city)
+
+score = 100
+language = "Python"
+print(score)
+print(language)
