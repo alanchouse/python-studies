c7ef60d107073d30d9dbdf7710934875f5f06314
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:11:58 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/comments.py
+++ Practice1/python-basics/comments.py
@@ -1,2 +1,8 @@
 # Это комментарий
 print("Hello, World!")
+
+print("Hello, Worldzzzz!")
+print("Hello, Python!")
+print("My name is Alan")
+print("I am learning Python")
+print("Welcome to my program")
