959f2c9e7ebd81f0b420ae8d772758adf518cfd0
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:15:12 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/numbers.py
+++ Practice1/python-basics/numbers.py
@@ -2,3 +2,28 @@ x = 1    # int
 y = 2.8  # float
 z = 1j   # complex
 print(x)
+
+x = 5
+y = 3.2
+z = 2j
+print(x)
+
+a = 10
+b = 4.5
+c = 1j
+print(a)
+
+num1 = 7
+num2 = 2.8
+num3 = 3j
+print(num1)
+
+x = 15
+y = 6.7
+z = 5j
+print(x)
+
+a = 20
+b = 9.5
+c = 4j
+print(a)
