e1c6a10885b08a8634b289cfcbedd18800632a6d
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:25:48 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/syntax.py
+++ Practice1/python-basics/syntax.py
@@ -1,2 +1,17 @@
 if 5 > 2:
     print("Five is greater than two!")
+
+if 10 > 5:
+    print("Ten is greater than five!")
+
+if 8 < 12:
+    print("Eight is less than twelve!")
+
+if 7 == 7:
+    print("Seven is equal to seven!")
+
+if 15 > 10:
+    print("Fifteen is greater than ten!")
+
+if 3 < 9:
+    print("Three is less than nine!")
