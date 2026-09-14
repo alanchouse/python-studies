9c78bc82e765592f3d08877af47929ce4ed86bc9
Alan Kagazov <alan.kagazov@gmail.com>
2026-09-14 21:24:58 +0500

    Add Practice1 Python basics examples

--- Practice1/python-basics/strings.py
+++ Practice1/python-basics/strings.py
@@ -1,3 +1,23 @@
 a = "Hello, World!"
 print(len(a))
 print(a[1])
+
+a = "Python"
+print(len(a))
+print(a[2])
+
+b = "Hello"
+print(len(b))
+print(b[1])
+
+c = "Computer"
+print(len(c))
+print(c[3])
+
+d = "Programming"
+print(len(d))
+print(d[5])
+
+e = "Almaty"
+print(len(e))
+print(e[2])
